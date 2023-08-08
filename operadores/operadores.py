278ee127fd07e6e6e18e521ba701a108f40f37f1
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-08 09:09:21 -0300

    Operadores Aritméticos, Relacionais e Lógicos

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,6 +1,7 @@
 a = 3
 b = -4
 
+# Operadores Aritméticos
 soma = a + b
 sub = a - b
 mult = a * b
@@ -14,3 +15,33 @@ print(f"Divisão: { div }")
 print(f"Divisão de inteiros: { div_inteiros }")
 print(f"Cálculos dentro do print: { a * b }")
 print(f"Resto da divisão de 16 por a: { 16 % a }")
+
+# Operadores Relacionais
+print(f"a == b: {a == b}")
+print(f"a < 5: {a < 5}")
+print(f"a > b: {a > b}")
+print(f"a <= 3: {a <= 3}")
+print(f"a >= 4: {a >= 4}")
+print(f"a != b: {a != b}")
+
+c = a != b
+print(f"c: {c}")
+print(f"tipo da var c: {type(c)}") # bool é o tipo booleano ou lógico
+
+# Operadores Lógicos
+logic1 = True
+logic2 = False
+print(f"type(logic1): {type(logic1)}")
+print(f"type(logic2): {type(logic2)}")
+
+print(f"logic1: {logic1}")
+print(f"not logic1: {not logic1}")
+print(f"logic1 or logic2: {logic1 or logic2}")
+print(f"logic1 and logic2: {logic1 and logic2}")
+
+
+
+
+
+
+
